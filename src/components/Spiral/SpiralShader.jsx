874fe5521178c36fb1dbab49bb97320d218bbda5
Daria Borisiak <daria.borisiak@gmail.com>
2026-05-28 18:05:58 +0200

    Add cosine palette

--- src/components/Spiral/SpiralShader.jsx
+++ src/components/Spiral/SpiralShader.jsx
@@ -5,15 +5,34 @@ import { RawShaderMaterial } from '../../utils/RawShaderMaterial'
 
 import vertexShader from './vertex.glsl'
 import fragmentShader from './fragment.glsl'
+import { useControls, folder } from 'leva'
 
 export function SpiralShader() {
     const { width, height } = useThree((state) => state.viewport)
     const materialRef = useRef(null)
 
+    const controls = useControls({
+        'Cosine Palette': folder({
+            uBias: { r: 0.86 * 255, g: 0.62 * 255, b: 0.55 * 255 },
+            uAmplitude: { r: 0.4 * 255, g: 0.41 * 255, b: 0.53 * 255 },
+            uFrequency: { x: 2.0, y: 1.0, z: 1.0 },
+            uPhase: { x: 0, y: 0.25, z: 0.25 },
+        }),
+        General: folder({
+            uSpeed: { value: 1.5, min: 0, max: 100, step: 0.01 },
+        }),
+    })
+
     const uniforms = useMemo(
         () => ({
-            uTime: { value: 0 },
+            uTime: new THREE.Uniform(0),
             uResolution: new THREE.Uniform(new THREE.Vector2(width, height)),
+            uSpeed: new THREE.Uniform(2),
+
+            uBias: new THREE.Uniform(new THREE.Vector3(0.86, 0.62, 0.55)),
+            uAmplitude: new THREE.Uniform(new THREE.Vector3(0.4, 0.41, 0.53)),
+            uFrequency: new THREE.Uniform(new THREE.Vector3(2, 1, 1)),
+            uPhase: new THREE.Uniform(new THREE.Vector3(0, 0.25, 0.25)),
         }),
         // eslint-disable-next-line react-hooks/exhaustive-deps
         []
@@ -23,8 +42,32 @@ export function SpiralShader() {
         if (materialRef.current) {
             materialRef.current.uniforms.uResolution.value.x = width
             materialRef.current.uniforms.uResolution.value.y = height
+
+            materialRef.current.uniforms.uSpeed.value = controls.uSpeed
+
+            materialRef.current.uniforms.uBias.value = new THREE.Vector3(
+                controls.uBias.r / 255,
+                controls.uBias.g / 255,
+                controls.uBias.b / 255
+            )
+            materialRef.current.uniforms.uAmplitude.value = new THREE.Vector3(
+                controls.uAmplitude.r / 255,
+                controls.uAmplitude.g / 255,
+                controls.uAmplitude.b / 255
+            )
+
+            materialRef.current.uniforms.uFrequency.value = new THREE.Vector3(
+                controls.uFrequency.x,
+                controls.uFrequency.y,
+                controls.uFrequency.z
+            )
+            materialRef.current.uniforms.uPhase.value = new THREE.Vector3(
+                controls.uPhase.x,
+                controls.uPhase.y,
+                controls.uPhase.z
+            )
         }
-    }, [width, height])
+    }, [width, height, controls])
 
     useFrame((state) => {
         if (materialRef.current) {
--- src/components/Spiral/fragment.glsl
+++ src/components/Spiral/fragment.glsl
@@ -2,14 +2,23 @@ precision mediump float;
 
 uniform vec2 uResolution;
 uniform float uTime;
+uniform vec3 uBias;
+uniform vec3 uAmplitude;
+uniform vec3 uFrequency;
+uniform vec3 uPhase;
+uniform float uSpeed;
 
 varying vec2 vUv;
+
+#define PI 3.1415926535
 
 float sdfCircle( vec2 position, float radius ) {
     return length(position) - radius;
 }
 
-#define PI 3.1415926535
+vec3 cosinePalette(float t, vec3 a, vec3 b, vec3 c, vec3 d) {
+    return a + b * cos(2.0 * PI * (c * t + d));
+}
 
 void main() {
     // 1. Move the center to (0.5, 0.5) by substracting 0.5
@@ -19,14 +28,20 @@ void main() {
     // Prevent from deformation on window resize
     uv.x *= uResolution.x / uResolution.y;
 
-    vec3 finalColor = vec3(1.0, 1.0, 2.0);
-
     // Create a circle
     float strength = sdfCircle(uv, 0.5);
 
+    vec3 finalColor = cosinePalette(
+        uTime * 0.2 + strength,
+        uBias,
+        uAmplitude,
+        uFrequency,
+        uPhase
+    );
+
     // Create a spiral
     float angle = atan(uv.y, uv.x);
-    strength = sin(strength * 16.0 - uTime * 2.0 + angle) / 2.0 + 0.5;
+    strength = sin(strength * 16.0 - uTime * uSpeed + angle) / 2.0 + 0.5;
 
     vec4 color = vec4(
         finalColor * strength,
