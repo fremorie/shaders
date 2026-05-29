c11d396ce690595c05fea9840258113688b9700b
Daria Borisiak <daria.borisiak@gmail.com>
2026-05-29 17:36:39 +0200

    Add credits section

--- src/components/Spiral/Page.jsx
+++ src/components/Spiral/Page.jsx
@@ -3,6 +3,7 @@ import { OrthographicCamera } from '@react-three/drei'
 import { useCreateStore, LevaPanel } from 'leva'
 
 import { SpiralShader } from './SpiralShader'
+import { Explanation } from '../layout/Explanation/Explanation'
 
 export function SpiralShaderPage() {
     const store = useCreateStore()
@@ -13,6 +14,21 @@ export function SpiralShaderPage() {
                 <SpiralShader store={store} />
                 <OrthographicCamera makeDefault position={[0, 0, 1]} />
             </Canvas>
+            <Explanation title="Credits">
+                <p>
+                    This shader uses a cosine palette implementation based on an
+                    article by Inigo Quilez:
+                </p>
+
+                <p>
+                    <a
+                        target="_blank"
+                        href="https://iquilezles.org/articles/palettes/"
+                    >
+                        iquilezles.org/articles/palettes
+                    </a>
+                </p>
+            </Explanation>
         </>
     )
 }
--- src/components/WavySpiral/Page.jsx
+++ src/components/WavySpiral/Page.jsx
@@ -3,6 +3,7 @@ import { OrthographicCamera } from '@react-three/drei'
 import { useCreateStore, LevaPanel } from 'leva'
 
 import { WavySpiralShader } from './WavySpiralShader'
+import { Explanation } from '../layout/Explanation/Explanation'
 
 export function WavySpiralShaderPage() {
     const store = useCreateStore()
@@ -16,6 +17,21 @@ export function WavySpiralShaderPage() {
                 <WavySpiralShader store={store} />
                 <OrthographicCamera makeDefault position={[0, 0, 1]} />
             </Canvas>
+            <Explanation title="Credits">
+                <p>
+                    This shader uses a cosine palette implementation based on an
+                    article by Inigo Quilez:
+                </p>
+
+                <p>
+                    <a
+                        target="_blank"
+                        href="https://iquilezles.org/articles/palettes/"
+                    >
+                        iquilezles.org/articles/palettes
+                    </a>
+                </p>
+            </Explanation>
         </>
     )
 }
