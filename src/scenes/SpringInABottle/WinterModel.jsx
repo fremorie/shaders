2ae2220a03702bdd0d0de8c8b821b8fec32ac0bd
Daria Borisiak <daria.borisiak@gmail.com>
2026-06-09 22:30:55 +0200

    Put winter scene in the bottle

--- src/scenes/SpringInABottle/WinterModel.jsx
+++ src/scenes/SpringInABottle/WinterModel.jsx
@@ -1,26 +1,46 @@
-import { useGLTF, useMask } from '@react-three/drei'
+import { useGLTF, useMask, useTexture } from '@react-three/drei'
 
 export function WinterModel() {
     const stencil = useMask(1, true)
 
-    const { nodes } = useGLTF('./models/Spring/Spring3.glb')
+    const { nodes } = useGLTF('./models/Winter/WinterMerged.glb')
+    const bakedTexture = useTexture('./models/Winter/BakedWinter.jpg')
+    // eslint-disable-next-line
+    bakedTexture.flipY = false
 
     return (
         <group rotation-y={Math.PI / 1.2} position-y={-0.5} dispose={null}>
             <mesh
-                geometry={nodes.merged.geometry}
-                position={[-0.871, 0.616, 0.325]}
-            >
-                <meshBasicMaterial {...stencil} />
-            </mesh>
-            <mesh
-                geometry={nodes.river.geometry}
+                castShadow
+                receiveShadow
+                geometry={nodes.river001.geometry}
                 position={[-0.251, 0.508, 0.127]}
             >
-                <meshBasicMaterial {...stencil} />
+                <meshPhysicalMaterial
+                    reflectivity={1}
+                    roughness={0}
+                    color="#767D93"
+                    {...stencil}
+                />
+            </mesh>
+            <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.snow.geometry}
+                position={[0.005, 0.284, -0.003]}
+            >
+                <meshBasicMaterial map={bakedTexture} {...stencil} />
+            </mesh>
+            <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.merged001.geometry}
+                position={[0.385, 0.863, -0.803]}
+            >
+                <meshBasicMaterial map={bakedTexture} {...stencil} />
             </mesh>
         </group>
     )
 }
 
-useGLTF.preload('./models/Spring/Spring3.glb')
+useGLTF.preload('./models/Winter/WinterMerged.glb')
