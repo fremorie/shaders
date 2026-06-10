a4d740f461a24d179dd6a48bc8106e8dd85407dd
Daria Borisiak <daria.borisiak@gmail.com>
2026-06-10 22:12:18 +0200

    Add a vignette-like shader on MagicGlass

--- src/scenes/SpringInABottle/shaders/magicGlass/fragment.glsl
+++ src/scenes/SpringInABottle/shaders/magicGlass/fragment.glsl
@@ -6,7 +6,15 @@ void main() {
     // Final color
     vec3 finalColor = vec3(0.5, 0.5, 1.0);
 
-    gl_FragColor = vec4(vec3(finalColor), 0.5);
+    // Move the center to (0.5, 0.5),
+    // then change range from [-0.5, 0.5] to [-1, 1]
+    vec2 uv = (vUv - 0.5) * 2.0;
+
+    float distanceToCenter = length(uv);
+    float strength = 1.0 / distanceToCenter - 0.5;
+    float alpha = 1.0 - strength;
+
+    gl_FragColor = vec4(finalColor, alpha);
 
     #include <colorspace_fragment>
 }
