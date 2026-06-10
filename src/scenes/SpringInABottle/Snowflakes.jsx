80afdb493c71432812bbd0f389711e9948857ebe
Daria Borisiak <daria.borisiak@gmail.com>
2026-06-10 14:58:52 +0200

    Switch to custom material

--- src/scenes/SpringInABottle/Snowflakes.jsx
+++ src/scenes/SpringInABottle/Snowflakes.jsx
@@ -1,16 +1,14 @@
-import { shaderMaterial, useMask } from '@react-three/drei'
-import * as THREE from 'three'
-import vertexShader from './shaders/river/vertex.glsl'
-import fragmentShader from './shaders/river/fragment.glsl'
 import { extend } from '@react-three/fiber'
+import * as THREE from 'three'
+import { shaderMaterial, useMask, useTexture } from '@react-three/drei'
+
+import vertexShader from './shaders/snowflakes/vertex.glsl'
+import fragmentShader from './shaders/snowflakes/fragment.glsl'
 
 const SnowflakeMaterial = shaderMaterial(
     {
         uTime: 0,
-        uEdgeColor: new THREE.Color('#dbd497'),
-        uDepthColor: new THREE.Color('#23938a'),
-        uDepthMap: null,
-        uPerlinNoise: null,
+        uTexture: null,
     },
     vertexShader,
     fragmentShader
@@ -31,6 +29,7 @@ for (let i = 0; i < particlesCount; i++) {
 
 export function Snowflakes() {
     const stencil = useMask(1)
+    const snowflakeTexture = useTexture('./textures/snowflake.png')
 
     return (
         <points>
@@ -42,10 +41,12 @@ export function Snowflakes() {
                     array={particlePositions}
                 />
             </bufferGeometry>
-            <pointsMaterial
-                size={0.04}
-                color="#ffffff"
+            <snowflakeMaterial
+                key={SnowflakeMaterial.key}
                 transparent
+                depthWrite={false}
+                blending={THREE.AdditiveBlending}
+                uTexture={snowflakeTexture}
                 {...stencil}
             />
         </points>
