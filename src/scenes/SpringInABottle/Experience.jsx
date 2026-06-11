898738ce359caf29ede34fcae315681d2efcaef1
Daria Borisiak <daria.borisiak@gmail.com>
2026-06-11 13:09:15 +0200

    Rotate the scene to match lights and shadows

--- src/scenes/SpringInABottle/Experience.jsx
+++ src/scenes/SpringInABottle/Experience.jsx
@@ -15,8 +15,10 @@ export function Experience({ store }) {
                 backgroundIntensity={0.5}
             />
 
-            <Float>
-                <group>
+            <Float speed={0.5}>
+                {/* Rotate the whole scene so that it looks like
+                the sun from the environment map casts shadows */}
+                <group rotation-y={-2.8}>
                     <BottleModel store={store} />
                     <Scene />
                     <CircularMask />
