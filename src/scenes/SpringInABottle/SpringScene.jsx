1d666ffa436f34686e6f902f74a2ab110869a4f4
Daria Borisiak <daria.borisiak@gmail.com>
2026-06-24 12:11:37 +0200

    Add boat model

--- src/scenes/SpringInABottle/SpringScene.jsx
+++ src/scenes/SpringInABottle/SpringScene.jsx
@@ -2,6 +2,7 @@ import { SpringModel } from './SpringModel'
 import { Butterflies } from './Butterflies'
 import { CherryBlossomPetals } from './CherryBlossomPetals'
 import { Grass } from './Grass'
+import { Boat } from './Boat'
 
 export function SpringScene({ store }) {
     return (
@@ -10,6 +11,7 @@ export function SpringScene({ store }) {
             <Butterflies store={store} />
             <CherryBlossomPetals store={store} />
             <Grass />
+            <Boat />
             {/*<DriftingPetal store={store} />*/}
         </>
     )
